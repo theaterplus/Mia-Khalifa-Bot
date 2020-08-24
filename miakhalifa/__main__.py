fd8465450f0db17b87940b4383bbaf58a6d87ebb
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-08-24 06:03:58 -0400

    Update __main__.py

--- miakhalifa/__main__.py
+++ miakhalifa/__main__.py
@@ -28,7 +28,7 @@ from miakhalifa.modules.connection import connect_button
 
 PM_START_TEXT = """
 _Hello_ *{}*
-_My name is_ *{}*\n_A Powerful Telegram ProBot to Manage Your Groups,feel free to add to your groups!!_
+_My name is_ *{}*\n_A Powerful Telegram ProBot to Manage only @TheaterPlusChat Group!!_
 _Maintained by_ [{}](tg://user?id={})
 """
 
@@ -78,7 +78,7 @@ GDPR = []
 
 START_IMG = os.environ.get('START_IMG', None)
 if START_IMG is None:
-    img = "https://telegra.ph/file/262c433b646b60a81cf72.jpg"
+    img = "https://telegra.ph/file/6af1681d9318bf931d620.jpg"
 else:
   img = START_IMG    
     
@@ -179,8 +179,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="🛡Creator🛡",url="https://t.me/TheUnusualPsychopath")]]
-    keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="⚜️Add Me⚜️",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="😇-HELP",callback_data="help_back"),InlineKeyboardButton(text="👨🏻‍💻-CREATOR",url="https://t.me/neil_arms")]]
+    keyboard += [[InlineKeyboardButton(text="💬-CONNECT GROUP", callback_data="main_connect"),InlineKeyboardButton(text="🌐-CHANNEL",url="https://t.me/Theater_Plus")]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
