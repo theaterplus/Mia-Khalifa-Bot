a0fb9f8b84729081e81a88784a8f0130d43515f9
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-08-24 10:50:41 -0400

    Update __main__.py

--- miakhalifa/__main__.py
+++ miakhalifa/__main__.py
@@ -27,9 +27,9 @@ from miakhalifa.modules.connection import connect_button
 
 
 PM_START_TEXT = """
-Hello *{}*
+Hello 👋🏻, *{}*
 My name is *{}*! I am a Pro Group Manager Bot that manage @TheaterPlusChat Group. Press the *HELP* button below to know more about me and my work!!
-*Made by* [{}](tg://user?id={})
+*MADE BY* [{}](tg://user?id={})
 """
 
 
@@ -38,12 +38,12 @@ Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
- 🔷 - /start: start the bot
- 🔷 - /help: PM's you this message.
- 🔷 - /help <module name>: PM's you info about that module.
- 🔷 - /settings:
-   🔹 - in PM: will send you your settings for all supported modules.
-   🔹 - in a group: will redirect you to pm, with all that chat's settings.
+ ⭕ - /start: start the bot
+ ⭕ - /help: PM's you this message.
+ ⭕ - /help <module name>: PM's you info about that module.
+ ⭕ - /settings:
+   🔺 - in PM: will send you your settings for all supported modules.
+   🔺 - in a group: will redirect you to pm, with all that chat's settings.
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
