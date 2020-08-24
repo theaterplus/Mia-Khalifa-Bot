af1fb85241efc1ebbd5e6ad9e15f490beffac743
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-08-24 05:03:33 -0400

    Update __main__.py

--- miakhalifa/__main__.py
+++ miakhalifa/__main__.py
@@ -41,7 +41,6 @@ the things I can help you with.
  ⭕️ - /start: start the bot
  ⭕️ - /help: PM's you this message.
  ⭕️ - /help <module name>: PM's you info about that module.
- ⭕️ - /source: Information about my source.
  ⭕️ - /settings:
    🔺 - in PM: will send you your settings for all supported modules.
    🔺 - in a group: will redirect you to pm, with all that chat's settings.
@@ -56,11 +55,6 @@ VERSION = "6.0"
 def vercheck() -> str:
     return str(VERSION)
 
-
-SOURCE_STRING = """
-⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/Adithyan1133-ctrl/Mia-Khalifa-Bot)
-⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://github.com/Adithyan1133-ctrl/Mia-Khalifa-Bot.git)
-"""
 
 
 IMPORTED = {}
@@ -78,7 +72,7 @@ GDPR = []
 
 START_IMG = os.environ.get('START_IMG', None)
 if START_IMG is None:
-    img = "https://telegra.ph/file/262c433b646b60a81cf72.jpg"
+    img = "https://telegra.ph/file/6af1681d9318bf931d620.jpg"
 else:
   img = START_IMG    
     
@@ -179,8 +173,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="🛡Creator🛡",url="https://t.me/TheUnusualPsychopath")]]
-    keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="⚜️Add Me⚜️",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="😇-HELP",callback_data="help_back"),InlineKeyboardButton(text="👨🏻‍💻-CREATOR",url="https://t.me/neil_arms")]]
+    keyboard += [[InlineKeyboardButton(text="🌐-CONNECT GROUP", callback_data="main_connect")
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
