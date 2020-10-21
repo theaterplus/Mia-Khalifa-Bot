6207db6c43c1de54d656fe7e4b331ebe6ac94172
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-10-21 18:31:49 +0530

    Update warns.py

--- miakhalifa/modules/warns.py
+++ miakhalifa/modules/warns.py
@@ -62,13 +62,13 @@ def warn(user: User, chat: Chat, reason: str, message: Message, warner: User = N
 
     else:
         
-        keyboard = InlineKeyboardMarkup([{InlineKeyboardButton("❌ Cancel",
+        keyboard = InlineKeyboardMarkup([{InlineKeyboardButton("❌  Cancel",
                                                                callback_data="rm_warn({})".format(user.id))}])
 
-        reply = f"{mention_html(user.id, user.first_name)} has been warned!"
+        reply = f"{mention_html(user.id, user.first_name)} has been <b>WARNED!</b>"
         if reason:
-            reply += f"\n• <b>Count : {num_warns}/{limit}"
-            reply += f"\n• <b>Reason : {html.escape(reason)}"
+            reply += f"\n• Count : {num_warns}/{limit}"
+            reply += f"\n• Reason : {html.escape(reason)}"
         
         log_reason = (f"<b>{html.escape(chat.title)}:</b>\n"
                       f"#WARNn<b>Admin:</b> {warner_tag}\n"
