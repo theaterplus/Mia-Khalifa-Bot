53b00680b50f3bb3717db37c33a9d04953011e19
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-10-23 16:51:17 +0530

    Update misc.py

--- miakhalifa/modules/helper_funcs/misc.py
+++ miakhalifa/modules/helper_funcs/misc.py
@@ -68,12 +68,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("👈🏻", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🏰", callback_data="bot_start"),
-             EqInlineKeyboardButton("👉🏻", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("🏡", callback_data="bot_start"),
+             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("🏰", callback_data="bot_start")]]
+        pairs += [[EqInlineKeyboardButton("🏡", callback_data="bot_start")]]
 
 
     return pairs
